1bb19f771e1dae1ba5f355a531f8cb49e55941a6
Mati18505 <mstulczewski@gmail.com>
2022-06-29 23:19:50 +0200

    Application: usunięto ustawianie window na nullptr - niepotrzebne, dodano dokładniejsze sprawdzanie błędów OpenGL

--- MatCore_R2/source/Application.cpp
+++ MatCore_R2/source/Application.cpp
@@ -8,10 +8,15 @@
 #include <crtdbg.h>
 #include "Scene.h"
 
+void GLAPIENTRY MessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
+{
+    fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n", 
+        (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""), type, severity, message);
+}
+
 Application::Application(){
     renderer = nullptr;
     scene = nullptr;
-    window = nullptr;
 }
 Application::~Application() {
     delete this->renderer;
@@ -49,6 +54,7 @@ void Application::InitGL() {
     gladLoadGL();
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
+    glEnable(GL_DEBUG_OUTPUT);
     glViewport(0, 0, windowWidth, windowHeight);
 }
 
@@ -66,4 +72,5 @@ void Application::MainLoop() {
     int error = glGetError();
     if (error != 0)
         std::cout<<"GLERROR: " << std::to_string(error) << std::endl;
+    glDebugMessageCallback(MessageCallback, 0);
 }
