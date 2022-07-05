8487789f760cfe2b5bff79d61d4ce78357be6954
Mati18505 <mstulczewski@gmail.com>
2022-07-05 10:49:52 +0200

    MeshComponent: zabroniono kopiowania, dozwolono move, usunięt plik cpp (niepotrzebny)

--- MatCore_R2/headers/MeshComponent.h
+++ MatCore_R2/headers/MeshComponent.h
@@ -5,7 +5,12 @@ struct MeshComponent {
 	MeshComponent(Mesh mesh) : isVAOCreated(false), VAO(0), VBO(0), EBO(0) {
 		this->mesh = mesh;
 	}
-	MeshComponent(const MeshComponent& other);
+	MeshComponent(const MeshComponent& other) = delete;
+	MeshComponent(MeshComponent&& other) noexcept = default;
+	
+	MeshComponent& operator=(const MeshComponent& other) = delete;
+	
+	MeshComponent& operator=(MeshComponent&& other) noexcept = default;
 
 
 	unsigned int VAO, VBO, EBO;
--- MatCore_R2/source/MeshComponent.cpp
+++
@@ -1,6 +0,0 @@
-#include "MeshComponent.h"
-#include "Log.h"
-
-MeshComponent::MeshComponent(const MeshComponent& other) {
-	LOG_CORE_WARN("MeshComponent copy constructor called!");
-}
