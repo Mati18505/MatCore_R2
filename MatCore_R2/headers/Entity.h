1c5c12b6bb0d42f629e63cd8d83093e9ac28d07a
Mati18505 <mstulczewski@gmail.com>
2022-07-12 11:07:53 +0200

    Entity: dodano sprawdzanie czy entity istnieje

--- MatCore_R2/headers/Entity.h
+++ MatCore_R2/headers/Entity.h
@@ -10,6 +10,10 @@ namespace MatCore {
 
 		template<typename T, typename... Args>
 		T& AddComponent(Args&&... args) {
+			if (!scene->entitiesRegistry.valid(*this)) {
+				LOG_CORE_CRITICAL("AddComponent: Entity was destroyed!");
+				assert("AddComponent: Entity was destroyed!");
+			}
 			if (HasComponent<T>()) {
 				LOG_CORE_WARN("AddComponent: Entity already has component!");
 				return GetComponent<T>();
@@ -19,6 +23,10 @@ namespace MatCore {
 
 		template<typename T>
 		T& GetComponent() {
+			if (!scene->entitiesRegistry.valid(*this)) {
+				LOG_CORE_CRITICAL("GetComponent: Entity was destroyed!");
+				assert("GetComponent: Entity was destroyed!");
+			}
 			if (!HasComponent<T>()) {
 				LOG_CORE_CRITICAL("GetComponent: Entity does not have component!");
 				assert("Entity does not have component!");
@@ -28,11 +36,20 @@ namespace MatCore {
 
 		template<typename T>
 		bool HasComponent() {
+			if (!scene->entitiesRegistry.valid(*this)) {
+				LOG_CORE_WARN("HasComponent: Entity was destroyed!");
+				return false;
+			}
+
 			return scene->entitiesRegistry.all_of<T>(entityHandle);
 		}
 
 		template<typename T>
 		void RemoveComponent() {
+			if (!scene->entitiesRegistry.valid(*this)) {
+				LOG_CORE_WARN("RemoveComponent: Entity was destroyed!");
+				return;
+			}
 			scene->entitiesRegistry.remove<T>(entityHandle);
 		}
 
