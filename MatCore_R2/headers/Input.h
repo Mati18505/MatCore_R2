208647f7c7f736ea88fa476379be6c8511e2906e
Mati18505 <mstulczewski@gmail.com>
2022-07-15 13:26:11 +0200

    Dodanoklasę abstrakcyjną Input, oraz WindowsInput który ją implementuje za pomocą GLFW

---
+++ MatCore_R2/headers/Input.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <utility>
+
+namespace MatCore {
+	class Input {
+	public:
+		static bool IsKeyPressed(int keyCode) { return instance->IsKeyPressedImpl(keyCode); }
+		static bool IsMouseButtonPressed(int button) { return instance->IsMouseButtonPressedImpl(button); }
+		static std::pair<float, float> GetMousePosition() { return instance->GetMousePositionImpl(); }
+		static float GetMouseX() { return instance->GetMouseXImpl(); }
+		static float GetMouseY() { return instance->GetMouseYImpl(); }
+
+	protected:
+		virtual bool IsKeyPressedImpl(int keyCode) = 0;
+
+		virtual bool IsMouseButtonPressedImpl(int button) = 0;
+		virtual std::pair<float, float> GetMousePositionImpl() = 0;
+		virtual float GetMouseXImpl() = 0;
+		virtual float GetMouseYImpl() = 0;
+
+	private:
+		friend class Application;
+		static Input* instance;
+	};
+}
---
+++ MatCore_R2/source/Platform/Windows/WindowsInput.cpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Platform/Windows/WindowsInput.h"
+#include "Application.h"
+#include <GLFW/glfw3.h>
+#include <MeshComponent.h>
+
+namespace MatCore {
+	bool WindowsInput::IsKeyPressedImpl(int keyCode)
+	{
+		auto window = applicationP->window;
+		auto state = glfwGetKey(window, keyCode);
+		return state == GLFW_PRESS || state == GLFW_REPEAT;
+	}
+	bool WindowsInput::IsMouseButtonPressedImpl(int button)
+	{
+		auto window = applicationP->window;;
+		auto state = glfwGetMouseButton(window, button);
+		return state == GLFW_PRESS;
+	}
+	std::pair<float, float> WindowsInput::GetMousePositionImpl()
+	{
+		auto window = applicationP->window;
+		double xpos, ypos;
+		glfwGetCursorPos(window, &xpos, &ypos);
+		return { (float)xpos, (float)ypos };
+	}
+	float WindowsInput::GetMouseXImpl()
+	{
+		auto [x, y] = GetMousePositionImpl();
+		return x;
+	}
+	float WindowsInput::GetMouseYImpl()
+	{
+		auto [x, y] = GetMousePositionImpl();
+		return y;
+	}
+}
