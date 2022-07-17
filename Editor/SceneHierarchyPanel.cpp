7002951ede394deae71fbb627c6710babf53131c
Mati18505 <mstulczewski@gmail.com>
2022-07-17 21:51:13 +0200

    Editor: SceneHierarchy: poprawiono wygląd komponentu Transform

--- Editor/SceneHierarchyPanel.cpp
+++ Editor/SceneHierarchyPanel.cpp
@@ -1,6 +1,64 @@
 #include "SceneHierarchyPanel.h"
 #include "Dependece/ImGUI/imgui.h"
+#include "Dependece/ImGUI/imgui_internal.h"
 #include "Editor.h"
+
+static void DrawVec3Control(const std::string& label, glm::vec3& values, float resetValue = 0.0f, float columnWidth = 100.f)
+{
+	ImGui::PushID(label.c_str()); //uwaga: label nie mo¿e siê powtórzyæ
+
+	ImGui::Columns(2);
+	ImGui::SetColumnWidth(0, columnWidth);
+	ImGui::Text(label.c_str());
+	ImGui::NextColumn();
+
+	ImGui::PushMultiItemsWidths(3, ImGui::CalcItemWidth());
+	ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, { 0, 0 });
+
+	float lineHeight = GImGui->Font->FontSize + GImGui->Style.FramePadding.y * 2.0f;
+	ImVec2 buttonSize = { lineHeight + 3.f, lineHeight };
+
+
+	ImGui::PushStyleColor(ImGuiCol_Button, { .8f, .1f, .15f, 1.f });
+	ImGui::PushStyleColor(ImGuiCol_ButtonHovered, { .9f, .2f, .2f, 1.f });
+	ImGui::PushStyleColor(ImGuiCol_ButtonActive, { .8f, .1f, .15f, 1.f });
+	if (ImGui::Button("X", buttonSize))
+		values.x = resetValue;
+	ImGui::PopStyleColor(3);
+
+	ImGui::SameLine();
+	ImGui::DragFloat("##X", &values.x, 0.1f);
+	ImGui::PopItemWidth();
+	ImGui::SameLine();
+	
+	ImGui::PushStyleColor(ImGuiCol_Button, { .2f, .7f, .2f, 1.f });
+	ImGui::PushStyleColor(ImGuiCol_ButtonHovered, { .3f, .8f, .3f, 1.f });
+	ImGui::PushStyleColor(ImGuiCol_ButtonActive, { .2f, .7f, .2f, 1.f });
+	if (ImGui::Button("Y", buttonSize))
+		values.y = resetValue;
+	ImGui::PopStyleColor(3);
+
+	ImGui::SameLine();
+	ImGui::DragFloat("##Y", &values.y, 0.1f);
+	ImGui::PopItemWidth();
+	ImGui::SameLine();
+	
+	ImGui::PushStyleColor(ImGuiCol_Button, { .1f, .25f, .8f, 1.f });
+	ImGui::PushStyleColor(ImGuiCol_ButtonHovered, { .2f, .35f, .9f, 1.f });
+	ImGui::PushStyleColor(ImGuiCol_ButtonActive, { .1f, .25f, .8f, 1.f });
+	if (ImGui::Button("Z", buttonSize))
+		values.z = resetValue;
+	ImGui::PopStyleColor(3);
+
+	ImGui::SameLine();
+	ImGui::DragFloat("##Z", &values.z, 0.1f);
+	ImGui::PopItemWidth();
+	
+	ImGui::PopStyleVar();
+
+	ImGui::Columns(1);
+	ImGui::PopID();
+}
 
 void SceneHierarchyPanel::Render(EditorScene* scene) {
 	using namespace MatCore;
@@ -68,9 +126,9 @@ void SceneHierarchyPanel::DrawInspector(MatCore::Entity entity)
 		if (ImGui::TreeNodeEx((void*)typeid(Transform).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform")) 
 		{
 			auto& transform = entity.GetComponent<Transform>();
-			ImGui::DragFloat3("Position", &transform.position[0], 0.1f);
-			ImGui::DragFloat3("Rotation", &transform.rotation[0], 0.1f);
-			ImGui::DragFloat3("Scale", &transform.scale[0], 0.1f);
+			DrawVec3Control("Position", transform.position, 0.f);
+			DrawVec3Control("Rotation", transform.rotation, 0.f);
+			DrawVec3Control("Scale", transform.scale, 1.f);
 
 			ImGui::TreePop();
 		}
