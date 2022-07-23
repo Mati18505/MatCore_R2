80404357269cf93244bf9ddff22186c4083347b9
Mati18505 <mstulczewski@gmail.com>
2022-07-23 20:42:14 +0200

    Material: dodano domyślną teksturę

--- MatCore_R2/source/Material.cpp
+++ MatCore_R2/source/Material.cpp
@@ -6,12 +6,14 @@
 MatCore::Material::Material(const char* vertexShaderPath, const char* fragmentShaderPath){
 	shaderID = Shader::Load(vertexShaderPath, fragmentShaderPath);
 	mvpMatrix = glm::mat4(1.f);
+	albedo = std::make_shared<Texture2D>("Resources/Textures/NoTexture.png");
 }
 
 MatCore::Material::Material(const Material& materialCopy) {
 	LOG_CORE_WARN("Material copy constructor called!");
 	this->mvpMatrix = materialCopy.mvpMatrix;
 	this->shaderID = materialCopy.shaderID;
+	albedo = std::make_shared<Texture2D>("Resources/Textures/NoTexture.png");
 }
 
 MatCore::Material::~Material()
