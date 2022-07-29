fadccca48ee5ee7c61f8a1e03ca07942ada49fba
Mati18505 <mstulczewski@gmail.com>
2022-07-30 00:22:52 +0200

    Dodano klasę EditorCamera

---
+++ Editor/EditorCamera.cpp
@@ -0,0 +1,81 @@
+#include "EditorCamera.h"
+#include <glm/gtx/quaternion.hpp>
+
+using namespace MatCore;
+double EditorCamera::mouseSensitivity = 0.001f;
+
+glm::quat EditorCamera::GetOrientation() const {
+	return glm::quat(glm::vec3(-pitch, -yaw, 0));
+}
+
+void EditorCamera::UpdateView() {
+	viewMatrix = glm::translate(glm::mat4(1.f), position) * glm::toMat4(GetOrientation());
+	viewMatrix = glm::inverse(viewMatrix);
+}
+
+void EditorCamera::UpdateProjection(){
+	projectionMatrix = glm::perspective(glm::radians(FOV), aspectRatio, 0.1f, 2000.f);
+}
+
+void EditorCamera::PanView(glm::vec2 offset)
+{
+	glm::normalize(offset);
+	double sensitivity = mouseSensitivity * 0.142857 * distance;
+		
+	center -= GetRightDirection() * offset.x * float(applicationP->deltaTime * sensitivity);
+	center -= GetUpDirection() * -offset.y * float(applicationP->deltaTime * sensitivity);
+}
+
+void EditorCamera::RotateView(glm::vec2 offset)
+{
+	glm::normalize(offset);
+	pitch += offset.y * applicationP->deltaTime * mouseSensitivity;
+	yaw += offset.x * applicationP->deltaTime * mouseSensitivity;
+	offsetDir = -GetForwardDirection();
+}
+
+void EditorCamera::Update() {
+	position = center + offsetDir * distance;
+	UpdateView();
+	UpdateProjection();
+}
+
+void EditorCamera::OnEvent(Event& e) {
+	EventDispatcher dispatcher(e);
+	dispatcher.Dispatch<MouseMovedEvent>([&](MouseMovedEvent& e) 
+	{
+		auto [x, y] = std::pair(e.GetX(), e.GetY());
+		glm::vec2 offset{ x - lastMousePos.x, y - lastMousePos.y };
+
+		if (Input::IsMouseButtonPressed(MC_MOUSE_BUTTON_3)) {
+			if (Input::IsKeyPressed(MC_KEY_LEFT_SHIFT))
+				PanView(offset);
+			else
+				RotateView(offset);
+		}
+		
+		lastMousePos = { x, y };
+		return true;
+	});
+	dispatcher.Dispatch<MouseScrolledEvent>([&](MouseScrolledEvent& e) 
+	{
+		distance += glm::pow(distance, 1.2)/20 * -e.GetYOffset() * applicationP->deltaTime/10;
+		distance = glm::max(distance, 1.5f);
+		return true;
+	});
+}
+
+glm::vec3 EditorCamera::GetUpDirection() const
+{
+	return glm::rotate(GetOrientation(), glm::vec3(0.0f, 1.0f, 0.0f));
+}
+
+glm::vec3 EditorCamera::GetRightDirection() const
+{
+	return glm::rotate(GetOrientation(), glm::vec3(1.0f, 0.0f, 0.0f));
+}
+
+glm::vec3 EditorCamera::GetForwardDirection() const
+{
+	return glm::rotate(GetOrientation(), glm::vec3(0.0f, 0.0f, -1.0f));
+}
---
+++ Editor/EditorCamera.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "MatCore.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+class EditorCamera : public MatCore::Camera {
+public:
+	EditorCamera() = default;
+	~EditorCamera() = default;
+
+	void OnEvent(MatCore::Event& e);
+	void Update();
+
+	glm::vec3 GetUpDirection() const;
+	glm::vec3 GetRightDirection() const;
+	glm::vec3 GetForwardDirection() const;
+	glm::quat GetOrientation() const;
+
+	float FOV = 90;
+	glm::vec3 center{ 0, 0, 0 };
+	float distance = 10;
+	static double mouseSensitivity;
+	float aspectRatio = 16.f / 9;
+
+private:
+	void UpdateView();
+	void UpdateProjection();
+	void PanView(glm::vec2 offset);
+	void RotateView(glm::vec2 offset);
+
+	float pitch = 0;
+	float yaw = 0;
+	glm::vec3 position{ 0, 0, 0 };
+
+	glm::vec2 lastMousePos{ 0, 0 };
+	glm::vec3 offsetDir{ 0, 0, 1 };
+};
