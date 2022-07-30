36dc107d14faf230d54bd35fef9daccd1a16df47
Mati18505 <mstulczewski@gmail.com>
2022-07-30 14:40:17 +0200

    NativeScriptComponent: dodano funkcję Get, zmienono instance na prywatny

--- MatCore_R2/headers/NativeScriptComponent.h
+++ MatCore_R2/headers/NativeScriptComponent.h
@@ -3,15 +3,18 @@
 
 namespace MatCore {
 	struct NativeScriptComponent {
-		ScriptableEntity* instance = nullptr;
-
 		template <typename T>
 		void Bind() {
 			InstantiateScript = []() {return static_cast<ScriptableEntity*>(new T()); };
 			DestroyScript = [](NativeScriptComponent* nsc) {delete nsc->instance; nsc->instance = nullptr; };
 		}
+		template <typename T>
+		T* Get() {
+			return static_cast<T*>(instance);
+		}
 
 	private:
+		ScriptableEntity* instance = nullptr;
 		ScriptableEntity* (*InstantiateScript)();
 		void(*DestroyScript)(NativeScriptComponent*);
 		friend class Scene;
